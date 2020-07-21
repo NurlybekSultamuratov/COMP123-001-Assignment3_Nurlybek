21fa58a6a91ce1b68d998a5a474da47b00feb4cc
Nurlybek Sultamuratov <nsultamu@my.centennialcollege.ca>
2020-07-21 12:29:18 -0500

    added fields to the planet class

---
+++ COMP123-001-Assignment3_Nurlybek/Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_001_Assignment3_Nurlybek
+{
+    abstract class Planet
+    {
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        public double Diameter
+        {
+            get => _diameter;
+        }
+    }
+}
